5c865612679a11af835ff0358670f5ccaad63495
Dwight Van der Velpen <Dfr34kz@gmail.com>
2019-03-24 18:23:30 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -7,18 +7,19 @@
 	define('DEBUG', true);
 
 	//Database Server Host
-	define ('DB_HOST', 'localhost');
+	define ('DB_HOST', 'eu-cdbr-west-02.cleardb.net');
 
 	// Database Server Username
-	define ('DB_USER', 'root');
+	define ('DB_USER', 'b49512ba806199');
 
 	// Database Server Password
-	define ('DB_PASS', 'root');
+	define ('DB_PASS', 'a06f6656');
 
 	// Database Name
-	define ('DB_NAME', 'mtgcollection');
+	define ('DB_NAME', 'heroku_76b3bbbcbcb75ae');
 
 	// database dump
 	define ('ERROR_LOG', __DIR__ . '/../error_log_mysql');
 
 // EOF
+// mysql://b49512ba806199:a06f6656@eu-cdbr-west-02.cleardb.net/heroku_76b3bbbcbcb75ae?reconnect=true
